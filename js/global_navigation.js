c3b9edf2f508cb87936f4004149b1596e4683de9
Nobuaki Miura <n.miura.0601@gmail.com>
2018-01-22 08:21:52 +0900

    update

--- js/global_navigation.js
+++ js/global_navigation.js
@@ -2,19 +2,6 @@ $(function() {
 	
 	var $header = $('#header');
 	
-	$(window).scroll(function() {
-		
-		if ($(window).scrollTop() > 50) {
-			
-			$header.addClass('fixed');
-			
-		} else {
-			
-			$header.removeClass('fixed');
-		}
-		
-	});
-	
 	var $wrapper = $('#wrapper');
 	
 	$('#toggle-menu').click(function(){
